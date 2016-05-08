4c7bf9daa5f6d65c4e2c335c3c278c53231b8707
Gabriel Almeida <gabrielgar.almeida@gmail.com>
2016-05-08 00:47:22 -0300

    pequenas melhorias e vários codigos incompletos ainda

--- BandejaBot.py
+++ BandejaBot.py
@@ -4,10 +4,46 @@ import time
 import telepot
 import Cardapio
 import logging
+import unicodedata
+import re
+
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 
 __author__ = 'gabriel'
 LOG_BOT = 'bandejaBot.log'
+MENSAGEM_START = """
+        Para obter o cardápio, use os comandos fornecidos ou simplesmente diga a palavras-chave abaixo:
+        <b>/almoco</b> e <b>/janta</b>: Retorna o cardápio do dia para o almoço e janta, respectivamente;
+        <b>/bandeja</b> ou qualquer comando não listado: Será respondido o cardápio da próxima refeição \
+        (apartir de 14h é considerado jantar);
+        <b>/semana</b>: Cardápio da um dia arbitrário da semana, que o bot irá perguntar, para uma resposta direta,\
+        use comandos como <i>Almoço de Segunda</i>;
+        <b>/help</b>: Exibe este diálogo.
+        """
+
+MENSAGEM_HORARIOS = """
+<i>De Segunda a Sexta</i>:
+<b>Central</b>:
+    <i>Almoço</i>: 11h-14h
+    <i>Jantar</i>: 17:30h-20:00h
+<b>CT</b>:
+    <i>Almoço</i>: 10:30h-14:30h
+    <i>Jantar</i>: 17:30h-20:00h
+<b>Letras</b>:
+    <i>Almoço</i>: 11:15h-14:00h
+    <i>Jantar</i>: 17:30h-20:00h
+
+<i>Fim de semana e Feriados</i> (Somente <b>Central</b>):
+    <i>Almoço</i>: 12:00h-14:00h
+    <i>Jantar</i>: 17:00h-19:15h
+"""
+
+REGEXP_CARACTERES_ACEITAVEIS = re.compile('[^a-z0-9 ]')
+REGEXP_ESPACOS = re.compile('[ ]+')
+REGEXP_CARDAPIO_SEMANA = re.compile('(%s) de (%s)' %
+                                    ("|".join(Cardapio.ORDEM_REFEICAO), "|".join(Cardapio.DIAS_DA_SEMANA)))
+
+
 
 class YourBot(telepot.Bot):
     logging.basicConfig(filename=LOG_BOT, level=logging.INFO,
@@ -15,23 +51,55 @@ class YourBot(telepot.Bot):
 
     def __init__(self, *args, **kwargs):
         super(YourBot, self).__init__(*args, **kwargs)
-        self._answerer = telepot.helper.Answerer(self)
         self.cardapio = Cardapio.Cardapio()
         self.cardapio.carrega_cardapio()
 
+    #par sensor-atuador
+
     def on_chat_message(self, msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
-        if 'almo' in msg['text'].lower():
-            response = self.cardapio.almoco_hoje()
-        elif 'janta' in msg['text'].lower():
-            response = self.cardapio.janta_hoje()
-        else:
-            response = self.cardapio.cardapio_mais_proximo()
+        if 'text' in msg:
+            txt = msg['text']
+            reply_markup = None
+            if '/start' in txt or '/help' in txt:
+              response = MENSAGEM_START
+            elif '/horarios' in txt:
+              response = MENSAGEM_HORARIOS
+            elif '/semana' in txt:
+                reply_markup = {'keyboard': [Cardapio.DIAS_DA_SEMANA[0:3], Cardapio.DIAS_DA_SEMANA[3:]]}
+                response = "Qual dia?"
+            elif txt in Cardapio.DIAS_DA_SEMANA:
+                reply_markup = {'keyboard': [[Cardapio.ORDEM_REFEICAO[0] + " de " + txt,
+                                              Cardapio.ORDEM_REFEICAO[1] + " de " + txt]]}
+                response = "Qual refeição?"
 
-        bot.sendMessage(chat_id, response, parse_mode='html')
-        print('Chat Message:', content_type, chat_type, msg['from']['first_name'] + " " + msg['from']['last_name'])
+            elif REGEXP_CARDAPIO_SEMANA.match(txt):
+                reply_markup = {'hide_keyboard': True}
+                resultado = txt.split(' de ')
+                response = self.cardapio.get_cardapio(resultado[0], resultado[1])
+            elif 'almo' in txt:
+                response = self.cardapio.almoco_hoje()
+            elif 'janta' in txt:
+                response = self.cardapio.janta_hoje()
+            else:
+                response = self.cardapio.cardapio_mais_proximo()
+
+            bot.sendMessage(chat_id, response, parse_mode='html', reply_markup=reply_markup)
+
         logging.info(msg)
 
+        try:
+            remetente = msg['from']['first_name'] + " " + msg['from']['last_name']
+        except:
+            remetente = chat_id
+
+        print('Chat Message:', content_type, chat_type, remetente)
+
+    def on_inline_query(self, msg):
+        pass
+
+    def on_chosen_inline_result(self, msg):
+        pass
 
 
 if __name__ == '__main__':
--- Cardapio.py
+++ Cardapio.py
@@ -2,7 +2,10 @@ import urllib.request
 from bs4 import BeautifulSoup
 import logging
 import datetime
-import string
+import sched
+import time
+import json
+import hashlib
 
 __author__ = 'gabriel'
 LOG_CARDAPIO = 'log/cardapio.log'
@@ -11,13 +14,8 @@ DIAS_DA_SEMANA = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', '
 ORDEM_CARDAPIO = ['Entrada', 'Prato Principal', 'Prato Vegetariano', 'Guarnição',
                   'Acompanhamento', 'Sobremesa', 'Refresco']
 ORDEM_REFEICAO = ['Almoço', 'Jantar']
-AJUSTE = max([ len(i) for i in ORDEM_CARDAPIO])
+#HORARIO_FUNCIONAMENTO =
 
-def compoe_mensagem(refeicao, dia_semana, dict_refeicao):
-    msg = "O %s de %s na UFRJ é:\n" % (refeicao.lower(), dia_semana.lower())
-    for prato in ORDEM_CARDAPIO:
-        msg += "<b>%s</b>: %s\n" % (prato, dict_refeicao[prato])
-    return msg
 
 
 class Cardapio():
@@ -26,6 +24,8 @@ class Cardapio():
 
     def __init__(self):
         self.cardapio = dict()
+        self.ultima_atualizacao = None
+        self.ultima_hash = 0
 
     def carrega_cardapio(self):
         with urllib.request.urlopen(CARDAPIO_URL) as response:
@@ -33,6 +33,7 @@ class Cardapio():
             logging.info("Cardapio lido de " + CARDAPIO_URL)
             soup = BeautifulSoup(html, 'html.parser')
 
+            cardapio_parseado = dict()
             contador = 0
             for elements in soup.find_all('td', 's3'):
                 id_dia_semana = contador % 7
@@ -43,9 +44,9 @@ class Cardapio():
                 prato_cardapio = ORDEM_CARDAPIO[id_cardapio]
                 refeicao = ORDEM_REFEICAO[id_refeicao]
 
-                if dia_semana not in self.cardapio:
-                    self.cardapio[dia_semana] = dict()
-                cardapios_dia = self.cardapio[dia_semana]
+                if dia_semana not in cardapio_parseado:
+                    cardapio_parseado[dia_semana] = dict()
+                cardapios_dia = cardapio_parseado[dia_semana]
 
                 if refeicao not in cardapios_dia:
                     cardapios_dia[refeicao] = dict()
@@ -54,9 +55,46 @@ class Cardapio():
                 if prato_cardapio not in cardapio_refeicao:
                     cardapio_refeicao[prato_cardapio] = dict()
                 cardapio_refeicao[prato_cardapio] = elements.contents[0]
-
                 contador += 1
-            logging.info("Cardápio: " + str(self.cardapio))
+
+            self.cardapio = cardapio_parseado
+
+            # Logica de atualizacao
+            cardapio_json = json.dumps(self.cardapio, sort_keys=True)
+            hash_cardapio = hashlib.sha1(cardapio_json.encode()).hexdigest()
+            if hash_cardapio != self.ultima_atualizacao:
+                self.ultima_hash = hash_cardapio
+                self.ultima_atualizacao = datetime.datetime.today()
+                logging.info("Cardápio atualizado: " + self.ultima_hash)
+                logging.info(cardapio_json)
+            else:
+                logging.info("Cardápio não atualizado, hash igual ao anterior")
+
+            # Agendador de atualizacao
+
+            # sc = sched.scheduler(time.time, time.sleep)
+            # proxima_atualizacao = self.__calcula_tempo_atualizacao()
+            # sc.enterabs(time=proxima_atualizacao, priority=1, action=self.carrega_cardapio)
+            # sc.run()
+            # logging.info("Proxima atualizacao agendada para ", proxima_atualizacao)
+
+    def __calcula_tempo_atualizacao(self):
+        # se ja atualizou na semana, entao proxima atualizacao agendada para segunda 8:00
+        proxima_segunda = datetime.timedelta(7 - self.ultima_atualizacao)
+
+        # senao conseguiu atualizar na segunda, entao proxima atualizacao agendada para daqui a 10 minutos
+
+        return 10
+
+    def calcula_data_cardapio(self, dia_semana):
+        '''
+            Calcula a data relativa a um dado dia da semana,
+            com relação a semana da ultima data de atualização do cardápio
+        '''
+        primeiro_dia_cardapio = self.ultima_atualizacao - datetime.timedelta(self.ultima_atualizacao.weekday())
+        offset_dia_semana = DIAS_DA_SEMANA.index(dia_semana)
+        data_dia_semana = (primeiro_dia_cardapio + datetime.timedelta(offset_dia_semana)).date()
+        return "%s/%s" % (data_dia_semana.day, data_dia_semana.month)
 
 
     def __horario(self):
@@ -67,10 +105,10 @@ class Cardapio():
             refeicao = 1
         else:
             refeicao = 0
-        return ORDEM_REFEICAO[refeicao], DIAS_DA_SEMANA[weekday-1]
+        return ORDEM_REFEICAO[refeicao], DIAS_DA_SEMANA[weekday]
 
     def get_cardapio(self, refeicao, dia):
-        return compoe_mensagem(refeicao, dia, self.cardapio[dia][refeicao])
+        return self.compoe_mensagem(refeicao, dia, self.cardapio[dia][refeicao])
 
     def janta_hoje(self):
         _, dia = self.__horario()
@@ -86,6 +124,12 @@ class Cardapio():
         refeicao, dia = self.__horario()
         return self.get_cardapio(refeicao, dia)
 
+    def compoe_mensagem(self, refeicao, dia_semana, dict_refeicao):
+        msg = "O %s de %s (%s) na UFRJ é:\n" % (refeicao.lower(), dia_semana.lower(),
+                                                self.calcula_data_cardapio(dia_semana))
+        for prato in ORDEM_CARDAPIO:
+            msg += "<b>%s</b>: %s\n" % (prato, dict_refeicao[prato])
+        return msg
 
 if __name__ == '__main__':
     c = Cardapio()
